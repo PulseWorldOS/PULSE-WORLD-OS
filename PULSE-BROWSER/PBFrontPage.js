fb338d4b31c00dfbe44e66f37aa291092ffb3edb
TropicPulse <aldwynfox101@gmail.com>
2026-09-07 17:04:27 -0700

    Adding Context Types for Search Engine

--- PULSE-BROWSER/PBFrontPage.js
+++ PULSE-BROWSER/PBFrontPage.js
@@ -10,18 +10,24 @@ document.getElementById("images").addEventListener("click", (event) => {
   const text = document.getElementById("searchengineTextbox").innerText.trim();
   url = "https://www.google.com/search?q=" + encodeURIComponent(text) + "&sca_esv=97ecd86c81018411&hl=en&udm=2&biw=1920&bih=945&sxsrf=APpeQnum89enEMVZATiLWD0yh0mMe6oPJg%3A1788824443834&ei=e0ufas3CMvLMkPIP5PPV2Qw&ved=2ahUKEwiN7Kis0t2WAxVyJkQIHeR5NcsQ4dUDegQIBhAN&uact=5&oq=fsdfsdf&gs_lp=Egtnd3Mtd2l6LWltZyIHZnNkZnNkZjIKEAAYgAQYigUYQzIPEAAYgAQYChgLGLEDGIMBMgUQABiABDIJEAAYgAQYChgLMgkQABiABBgKGAsyBRAAGIAEMgkQABiABBgKGAsyCRAAGIAEGAoYCzIJEAAYgAQYChgLMgkQABiABBgKGAtIqANQAFgAcAF4AJABAJgBAKABAKoBALABALgBA8gBAJgCAaACAZgDAOIDBBgAIF3iAwQYACBe4gMEGAAgX-IDBBgAIGDiAwQYACBh4gMEGAAgYogGAZIHATGgBwCyBwC4BwDCBwMwLjHIBwGACAE&sclient=gws-wiz-img";
   document.getElementById("images").style.backgroundColor = "red";
+  document.getElementById("videos").style.backgroundColor = "black";
+  document.getElementById("text").style.backgroundColor = "black";
 });
 
 document.getElementById("videos").addEventListener("click", (event) => {
   const text = document.getElementById("searchengineTextbox").innerText.trim();
   url = "https://www.google.com/search?q=" + encodeURIComponent(text) + "&sca_esv=97ecd86c81018411&udm=7&biw=1920&bih=945&sxsrf=APpeQnuWOgRcMsUytLVkUMU_oHzKtkSHyw%3A1788824889059&ei=OU2fao2bA7TVkPIP4qfi4AI&ved=2ahUKEwjNm8-A1N2WAxW0KkQIHeKTGCwQ4dUDegQIBRAM&uact=5&oq=canva&gs_lp=EhZnd3Mtd2l6LW1vZGVsZXNzLXZpZGVvIgVjYW52YTIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwA0iuAVAAWABwAXgBkAEAmAEAoAEAqgEAsAEAuAEDyAEAmAIBoAICmAMA4gMEGAAgXeIDBBgAIF7iAwQYACBf4gMEGAAgYOIDBBgAIGHiAwQYACBiiAYBkAYIkgcBMaAHALIHALgHAMIHAzAuMcgHAYAIAQ&sclient=gws-wiz-modeless-video";
   document.getElementById("videos").style.backgroundColor = "red";
+  document.getElementById("images").style.backgroundColor = "black";
+  document.getElementById("text").style.backgroundColor = "black";
 });
 
 document.getElementById("text").addEventListener("click", (event) => {
   const text = document.getElementById("searchengineTextbox").innerText.trim();
   url = "https://www.google.com/search?q=" + encodeURIComponent(text);
   document.getElementById("text").style.backgroundColor = "red";
+  document.getElementById("images").style.backgroundColor = "black";
+  document.getElementById("videos").style.backgroundColor = "black";
 });
 
 document.getElementById("search").addEventListener("click", (event) => {
