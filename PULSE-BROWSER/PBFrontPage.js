fd9ccc9069b2634f1dae9e536cf2563b9ecbf3c3
TropicPulse <aldwynfox101@gmail.com>
2026-09-08 00:46:29 -0700

    df

--- PULSE-BROWSER/PBFrontPage.js
+++ PULSE-BROWSER/PBFrontPage.js
@@ -75,9 +75,15 @@ async function updateModuleIcons() {
   document.getElementById("moduleFav2Icon").style.backgroundImage = "";
   
 }
+function cleanQuery(query) {
+  // Remove ZERO‑WIDTH characters that cause "undefineddfg"
+  return query.replace(/[\u200B-\u200D\uFEFF]/g, "");
+}
 
 function buildSearchURL(engineURL, query) {
-  const q = query;
+  query = cleanQuery(query);     // ← THIS removes the symbol
+  const q = encodeURIComponent(query);
+
 
   // Normalize URL
   const url = engineURL.toLowerCase();
