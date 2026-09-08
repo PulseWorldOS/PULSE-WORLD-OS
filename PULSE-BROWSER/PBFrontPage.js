598bdf062ea8c24e741464865bc44a9c105e1b5c
TropicPulse <aldwynfox101@gmail.com>
2026-09-08 01:29:06 -0700

    Fixed Engine Ssearcch i thin

--- PULSE-BROWSER/PBFrontPage.js
+++ PULSE-BROWSER/PBFrontPage.js
@@ -184,7 +184,7 @@ document.getElementById("searchus").addEventListener("click", (event) => {
 });
 
 document.getElementById("navigate").addEventListener("click", () => {
-  let text = document.getElementById("searchengineTextbox").value.trim();
+  let text = document.getElementById("searchengineTextbox").textContent.trim();
 
   // If ANY space exists → not navigation
   if (text.includes(" ")) {
@@ -203,7 +203,7 @@ document.getElementById("navigate").addEventListener("click", () => {
 
 
 document.getElementById("images").addEventListener("click", (event) => {
-  const text = document.getElementById("searchengineTextbox").value.trim();
+  const text = document.getElementById("searchengineTextbox").textContent.trim();
   url = engineURL + encodeURIComponent(text) + "&sca_esv=97ecd86c81018411&hl=en&udm=2&biw=1920&bih=945&sxsrf=APpeQnum89enEMVZATiLWD0yh0mMe6oPJg%3A1788824443834&ei=e0ufas3CMvLMkPIP5PPV2Qw&ved=2ahUKEwiN7Kis0t2WAxVyJkQIHeR5NcsQ4dUDegQIBhAN&uact=5&oq=fsdfsdf&gs_lp=Egtnd3Mtd2l6LWltZyIHZnNkZnNkZjIKEAAYgAQYigUYQzIPEAAYgAQYChgLGLEDGIMBMgUQABiABDIJEAAYgAQYChgLMgkQABiABBgKGAsyBRAAGIAEMgkQABiABBgKGAsyCRAAGIAEGAoYCzIJEAAYgAQYChgLMgkQABiABBgKGAtIqANQAFgAcAF4AJABAJgBAKABAKoBALABALgBA8gBAJgCAaACAZgDAOIDBBgAIF3iAwQYACBe4gMEGAAgX-IDBBgAIGDiAwQYACBh4gMEGAAgYogGAZIHATGgBwCyBwC4BwDCBwMwLjHIBwGACAE&sclient=gws-wiz-img";
   document.getElementById("images").style.backgroundColor = "red";
   document.getElementById("videos").style.backgroundColor = "black";
@@ -212,7 +212,7 @@ document.getElementById("images").addEventListener("click", (event) => {
 });
 
 document.getElementById("videos").addEventListener("click", (event) => {
-  const text = document.getElementById("searchengineTextbox").value.trim();
+  const text = document.getElementById("searchengineTextbox").textContent.trim();
   url = engineURL + encodeURIComponent(text) + "&sca_esv=97ecd86c81018411&udm=7&biw=1920&bih=945&sxsrf=APpeQnuWOgRcMsUytLVkUMU_oHzKtkSHyw%3A1788824889059&ei=OU2fao2bA7TVkPIP4qfi4AI&ved=2ahUKEwjNm8-A1N2WAxW0KkQIHeKTGCwQ4dUDegQIBRAM&uact=5&oq=canva&gs_lp=EhZnd3Mtd2l6LW1vZGVsZXNzLXZpZGVvIgVjYW52YTIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwAzIKEAAYRxjWBBiwA0iuAVAAWABwAXgBkAEAmAEAoAEAqgEAsAEAuAEDyAEAmAIBoAICmAMA4gMEGAAgXeIDBBgAIF7iAwQYACBf4gMEGAAgYOIDBBgAIGHiAwQYACBiiAYBkAYIkgcBMaAHALIHALgHAMIHAzAuMcgHAYAIAQ&sclient=gws-wiz-modeless-video";
   document.getElementById("videos").style.backgroundColor = "red";
   document.getElementById("images").style.backgroundColor = "black";
@@ -221,7 +221,7 @@ document.getElementById("videos").addEventListener("click", (event) => {
 });
 
 document.getElementById("text").addEventListener("click", (event) => {
-  const text = document.getElementById("searchengineTextbox").value.trim();
+  const text = document.getElementById("searchengineTextbox").textContent.trim();
   url = buildSearchURL(engineURL, text);
   document.getElementById("text").style.backgroundColor = "red";
   document.getElementById("images").style.backgroundColor = "black";
@@ -230,7 +230,7 @@ document.getElementById("text").addEventListener("click", (event) => {
 });
 
 document.getElementById("search").addEventListener("click", (event) => {
-  let text = document.getElementById("searchengineTextbox").value.trim();
+  let text = document.getElementById("searchengineTextbox").textContent.trim();
 
   // Remove spaces just in case (e.g., "gmail . com")
   const cleaned = text.replace(/\s+/g, "");
@@ -270,7 +270,7 @@ document.getElementById("search").addEventListener("click", (event) => {
 
 
 document.getElementById("searchengineTextbox").addEventListener("input", () => {
-  let text = document.getElementById("searchengineTextbox").value.trim();
+  let text = document.getElementById("searchengineTextbox").textContent.trim();
   const navigateIcon = document.getElementById("navigate");
 
   // If ANY space exists → it's a search query
@@ -318,7 +318,7 @@ document.getElementById("searchengineTextbox").addEventListener("keydown", (even
   if (event.key === "Enter") {
     event.preventDefault();
 
-    let text = document.getElementById("searchengineTextbox").value.trim();
+    let text = document.getElementById("searchengineTextbox").textContent.trim();
 
     // Domain or domain + path
     const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/.*)?$/;
