2b85dc4ec882f84a9c6e46fdfbcfbcb070af7002
TropicPulse <aldwynfox101@gmail.com>
2026-09-13 01:30:06 -0700

    Testing LocalId Again

--- PULSE-SERVER/NETLIFY/PULSE-SERVER-SQL.js
+++ PULSE-SERVER/NETLIFY/PULSE-SERVER-SQL.js
@@ -198,7 +198,7 @@ export async function handler(event) {
           body: JSON.stringify({ ok: false, error: "Missing identity.id" })
         };
       }
-      
+
       const client = getSupabase();
 
       const now = new Date().toISOString();
@@ -231,7 +231,8 @@ export async function handler(event) {
         lastUpdated: now,
         host,
         online,
-        inactive
+        inactive,
+        localId: identity.id
       };
 
       let result;
