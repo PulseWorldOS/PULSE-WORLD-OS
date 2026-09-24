068a9871b8464e05f57c5d84d72e959446473df1
TropicPulse <aldwynfox101@gmail.com>
2026-09-23 23:25:21 -0700

    attacfhDocument

--- PULSE-BROWSER/PBCompanion.js
+++ PULSE-BROWSER/PBCompanion.js
@@ -478,7 +478,7 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
       if (msg.url) {
         pbAccelerate(msg.url);
       }
-      PBQuantumPrefetch.attachToDocument(document);
+      PBQuantumPrefetch.attachToDocument(msg.url);
       sendResponse({ ok: true });
       break;
 
